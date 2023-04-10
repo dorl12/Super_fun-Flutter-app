9d4ed1ad9545f3d6004afa1ce70dc14da94aa4ac
EliZilber <elisilverstine@gmail.com>
2023-04-10 12:47:12 +0300

    starting implementin db

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:numberpicker/numberpicker.dart';
 
-void main() => runApp(const MyApp());
+import 'navigation.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -33,20 +39,33 @@ class _MyCustomFormState extends State<MyCustomForm> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Row(
-        children: const <Widget>[
-          Expanded(
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter an item name',
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(20.0,100.0,20.0,0.0),
+      child: Column(
+        children: [
+          Row(
+            children: const <Widget>[
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter an item name',
+                  ),
+                ),
               ),
-            ),
+              SizedBox(width: 10),
+              IconButton(onPressed: AsyncSnapshot.nothing, icon: Icon(Icons.add_shopping_cart_rounded)),
+          ]
           ),
-          IconButton(onPressed: AsyncSnapshot.nothing, icon: Icon(Icons.add_shopping_cart_rounded)),
-      ]
+          const SizedBox(height: 10),
+          TextButton(onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => navigation()),
+            );
+          },child: Text('Start Shopping'),)
+        ],
       ),
-      );
+    );
   }
 }
